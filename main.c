e6d6d9d255fbd1a6db31caa8dd82aa047d0c9ee4
Joshua Faria <Akasen1226@yahoo.com>
2015-01-29 03:50:04 -0500

    Final Commit
    So for the most part, it works now. Sadly, I could not tell you what
    changes I have made between now and the last commit. Hopefully, my next
    one is essentially me explaining everything.

--- main.c
+++ main.c
@@ -1,103 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
 
-int stat_roll();
+int statCalc(int dice);
+int diceRoll();
+void operation(int dice);
 
 int main()
 {
-    int stats[6];
     printf("Welcome to the Stat Roller!\n");
-    printf("This Stat Roller uses the Pathfinder Standard Roll to determine stats\n");
-    printf("Do remember that these stats may be re-arranged at the players choice unless the GM says otherwise");
+    printf("This Stat Roller can use the Standard, Classic, or Heroic rolls to determine stats\n");
 
-    srand(time(NULL));
-    char player_continue[4];
+    int choice;
+    int dice;
+
     do
     {
-        // Rolls for the six player stats using stat_roll function
-        int x;
-        for(x = 0; x <= 5; x++)
-        {
-            stats[x] = stat_roll();
-        }
+        printf("\n1. Standard\n2. Classic\n3. Heroic\nChoose a roll type: ");
+        choice = 9;
+        scanf("%i", &choice);
 
-        // Prints all stats on one line. Create new blank line
-        printf("\n%i, %i, %i, %i, %i, %i\n",
-                stats[0], stats[1], stats[2], stats[3], stats[4], stats[5]);
-
-        // Asks user if they wish to continue
-        // At the moment, anything will allow the programm to roll again
-        // Only "no" can be used as the exit statement
-        printf("Enter \'no\' to exit. Enter \'yes\' to continue: ");
-        scanf("%s", &player_continue);
-    }
-    while(strcmp(player_continue, "no"));
+            if(choice == 1)
+            {
+                dice = 4;
+                operation(dice);
+            }
+            if(choice == 2)
+            {
+                int dice = 3;
+                operation(dice);
+            }
+            if(choice == 3)
+            {
+                int dice = 2;
+                operation(dice);
+            }
+    }while(choice != 0);
     return 0;
 }
 
-int stat_roll()
+void operation(int dice)
 {
-    // Initaliztion of varaibles
-    int roll1, roll2, roll3, roll4;
+    int stats[6];
+    char player_continue[4];
 
-    // Variables have random values between 'one' and 'six' set to them
-    // Variables then placed into an array
-    roll1 = rand()%6+1, roll2 = rand()%6 + 1, roll3 = rand()%6 + 1, roll4 = rand()%6 + 1;
-    int rolls[4] = {roll1, roll2, roll3, roll4};
-
-    // Initializing search for lowest variable
-
-    // The idea is that this will look for the smallest number using the first variable in the array
-    // If any other variable is smaller, that variable will be set to small
-    int x, small = rolls[0];
-    for(x = 0; x < 4; x++)
-    {
-        if (rolls[x] < small)
+    do
         {
-            small = rolls[x];
+            // Rolls for the six player stats using stat_roll function
+            int x;
+            for(x = 0; x <= 5; x++)
+            {
+                stats[x] = statCalc(dice);
+            }
 
-        }
+            // Prints all stats on one line. Create new blank line
+            printf("\n%i, %i, %i, %i, %i, %i\n",
+                    stats[0], stats[1], stats[2], stats[3], stats[4], stats[5]);
 
-    }
-
-    // Debug code. Keeping in for any further use
-    //printf("Small is %i\n", small);
-
-
-    // The moment the smallest number is found, it is set to '0'
-    // Break statement is done in the case that there are numbers of
-    // identical value to the 'small' variable, those numbers do not get
-    // made into a '0'
-    for(x = 0; x < 4; x++)
-    {
-        if(rolls[x] == small)
-        {
-            rolls[x] = 0;
-            break;
-        }
-    }
-
-    // Debug code. Keeping it in for any further use
-    //printf("%i, %i, %i, %i\n\n", rolls[0],rolls[1],rolls[2], rolls[3]);
-
-    // Addition of all numbers
-    int final_stat = rolls[0] + rolls[1] + rolls[2] + rolls[3];
-
-    return final_stat;
-
+            // Asks user if they wish to continue
+            // At the moment, anything will allow the program to roll again
+            printf("Enter \'n\' to exit. Enter \'y\' to continue: ");
+            scanf("%s", player_continue);
+        }while(strcmp(player_continue, "n"));
 }
+
+
+int diceRoll()
+{
+    return rand()%6+1;
+}
+
+
+int statCalc(int dice)
+{
+    // Initialization of variables
+    // i and total
+    int i, total, small;
+
+    total = small = diceRoll();
+    //For loop
+    for(i = 1; i < dice; i++) {
+        int roll = diceRoll();
+        total += roll;
+    }
+    if (dice == 2) { total += 6; }
+    else if (dice == 4) { total -= small; }
+    return total;
+}
+
 
 /*
 
 Particular issue has been realized.
-I wish to broadent the scope of this so that Pathfinder/D&D's other systems
+I wish to broaden the scope of this so that Pathfinder/D&D's other systems
 for rolling stats is taken into account as well. Future steps would be to
 add a GUI interface on top of all this. What may need to be done is to have the
 function 'stat_roll' accept an X amount for the variable size for rolls and then to have
 the search functions also call upon this size.
 
-To elabore, Pathfinder detailes at least three basic roll systems for Ability Score generation.
+To elaborate, Pathfinder details at least three basic roll systems for Ability Score generation.
 
 Standard makes you roll 4d6 and discard the lowest, adding the rest of the numbers.
 
@@ -111,3 +112,4 @@ Or maybe I could have an if statement that checks the variable for dice amount
 and signifies from there whether a flat six needs to be added to the roll.
 
 */
+
